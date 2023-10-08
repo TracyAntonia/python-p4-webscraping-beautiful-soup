872cdb73ffa38d3637fd6baf94ce6231d04084a1
TracyAntonia <aantonjuguna@gmail.com>
2023-10-08 22:29:19 +0300

    completed assignments

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,10 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://moringaschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+doc.select('.heading-60-black.color-black.mb-20')[0].attrs
+courses = doc.select('.heading-60-black.color-black.mb-20')[0]
+
+print(doc.select('.heading-60-black.color-black.mb-20'))
